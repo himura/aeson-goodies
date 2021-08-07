c2a0b719411c858eba3562396b777db604cd3926
Takahiro HIMURA <taka@himura.jp>
2021-08-07 22:47:15 +0900

    Change name StringInt -> IntegralString

---
+++ src/Data/Aeson/Goodies/IntegralString.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Data.Aeson.Goodies.IntegralString
+    ( IntegralString(..)
+    , IntString
+    ) where
+
+import Data.Aeson.Goodies.Util
+import Data.Aeson.Types hiding (Parser)
+import Data.Attoparsec.Text
+import Data.Ix
+import Data.Scientific
+import qualified Data.Text as T
+import GHC.Generics
+
+type IntString = IntegralString Int
+
+newtype IntegralString a =
+    IntegralString
+        { unIntegralString :: a
+        }
+    deriving ( Bounded
+             , Enum
+             , Eq
+             , Foldable
+             , FromJSONKey
+             , Functor
+             , Generic
+             , Integral
+             , Ix
+             , Num
+             , Ord
+             , Real
+             , Show
+             , Traversable
+             )
+
+instance (Bounded a, Integral a) => FromJSON (IntegralString a) where
+    parseJSON = prependContext "IntegralString" . p
+      where
+        p (Number n)
+            | Just i <- toBoundedInteger n = pure $ IntegralString i
+            | otherwise = fail $ "fail to convert number to integer: " ++ show n
+        p (String s) = do
+            case parseOnly (intParser <* endOfInput) s of
+                Left err -> fail $ "fail to parse as integer: " ++ T.unpack s ++ ", reason: " ++ err
+                Right n -> pure $ IntegralString n
+        p v = typeMismatch "Number or String" v
+
+instance Show a => ToJSON (IntegralString a) where
+    toJSON (IntegralString n) = String . T.pack . show $ n
+
+intParser :: Integral a => Parser a
+intParser = signed decimal
--- test/Spec.hs
+++ test/Spec.hs
@@ -9,7 +9,7 @@ import Test.Tasty.HUnit
 
 import Data.Aeson
 import Data.Aeson.Goodies.NumBool
-import Data.Aeson.Goodies.StringInt
+import Data.Aeson.Goodies.IntegralString
 
 main :: IO ()
 main = defaultMain $ testGroup "Tests" [numBoolTests, stringIntTests]
@@ -34,17 +34,17 @@ numBoolTests =
 stringIntTests :: TestTree
 stringIntTests =
     testGroup
-        "StringInt Int"
-        [ testCase "valid:   number 43" $ (decode "[43]" :: Maybe [StringInt Int]) @?= Just [StringInt 43]
-        , testCase "valid:   number -100" $ (decode "[-100]" :: Maybe [StringInt Int]) @?= Just [StringInt (-100)]
-        , testCase "invalid: number 1.5" $ (decode "[1.5]" :: Maybe [StringInt Int]) @?= Nothing
-        , testCase "invalid: number -100.5" $ (decode "[-100.5]" :: Maybe [StringInt Int]) @?= Nothing
-        , testCase "valid:   number 1e10" $ (decode "[1e10]" :: Maybe [StringInt Int]) @?= Just [StringInt 10000000000]
-        , testCase "valid:   number 1e50000000" $ (decode "[1e50000000]" :: Maybe [StringInt Int]) @?= Nothing -- reject huge exponents
-        , testCase "valid:   string 43" $ (decode "[\"43\"]" :: Maybe [StringInt Int]) @?= Just [StringInt 43]
-        , testCase "valid:   string -100" $ (decode "[\"-100\"]" :: Maybe [StringInt Int]) @?= Just [StringInt (-100)]
-        , testCase "invalid: string 1.5" $ (decode "[\"1.5\"]" :: Maybe [StringInt Int]) @?= Nothing
-        , testCase "invalid: string -100.5" $ (decode "[\"-100.5\"]" :: Maybe [StringInt Int]) @?= Nothing
-        , testCase "invalid: string 1e10" $ (decode "[\"1e10\"]" :: Maybe [StringInt Int]) @?= Nothing
-        , testCase "invalid: string 1e-10" $ (decode "[\"1e10\"]" :: Maybe [StringInt Int]) @?= Nothing
+        "IntegralString Int"
+        [ testCase "valid:   number 43" $ (decode "[43]" :: Maybe [IntegralString Int]) @?= Just [IntegralString 43]
+        , testCase "valid:   number -100" $ (decode "[-100]" :: Maybe [IntegralString Int]) @?= Just [IntegralString (-100)]
+        , testCase "invalid: number 1.5" $ (decode "[1.5]" :: Maybe [IntegralString Int]) @?= Nothing
+        , testCase "invalid: number -100.5" $ (decode "[-100.5]" :: Maybe [IntegralString Int]) @?= Nothing
+        , testCase "valid:   number 1e10" $ (decode "[1e10]" :: Maybe [IntegralString Int]) @?= Just [IntegralString 10000000000]
+        , testCase "valid:   number 1e50000000" $ (decode "[1e50000000]" :: Maybe [IntegralString Int]) @?= Nothing -- reject huge exponents
+        , testCase "valid:   string 43" $ (decode "[\"43\"]" :: Maybe [IntegralString Int]) @?= Just [IntegralString 43]
+        , testCase "valid:   string -100" $ (decode "[\"-100\"]" :: Maybe [IntegralString Int]) @?= Just [IntegralString (-100)]
+        , testCase "invalid: string 1.5" $ (decode "[\"1.5\"]" :: Maybe [IntegralString Int]) @?= Nothing
+        , testCase "invalid: string -100.5" $ (decode "[\"-100.5\"]" :: Maybe [IntegralString Int]) @?= Nothing
+        , testCase "invalid: string 1e10" $ (decode "[\"1e10\"]" :: Maybe [IntegralString Int]) @?= Nothing
+        , testCase "invalid: string 1e-10" $ (decode "[\"1e10\"]" :: Maybe [IntegralString Int]) @?= Nothing
         ]
